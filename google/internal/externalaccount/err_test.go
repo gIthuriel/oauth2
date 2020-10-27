b6aeeba1dccbda2a1888d73c7e5885b0ad6bd814
Patrick Jones <ithuriel@google.com>
2020-10-27 19:02:53 +0000

    Add utilities supporting upcoming oauth2 functionality.
    google: add extended utilities to help with STS requests.
    
    Change-Id: Iafc145b06ca42374cfc2ac6572762a50bcf560f2
    GitHub-Last-Rev: 6513477b5bc23d2bbcda8c180f59d236706b219a
    GitHub-Pull-Request: golang/oauth2#439

---
+++ google/internal/externalaccount/err_test.go
@@ -0,0 +1,15 @@
+package externalaccount
+
+import "testing"
+
+func TestError(t *testing.T) {
+	e := Error{
+		"42",
+		"http:thisIsAPlaceholder",
+		"The Answer!",
+	}
+	want := "got error code 42 from http:thisIsAPlaceholder: The Answer!"
+	if got := e.Error(); got != want {
+		t.Errorf("Got error message %q; want %q", got, want)
+	}
+}
