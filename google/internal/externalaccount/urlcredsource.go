d785f47b1539a875f439fcc1806bb532836fa827
Patrick Jones <ithuriel@google.com>
2020-10-29 11:03:54 -0700

    temp

---
+++ google/internal/externalaccount/urlcredsource.go
@@ -0,0 +1,10 @@
+package externalaccount
+
+type urlCredentialSource struct {
+	URL string
+	Headers map[string]interface{}
+}
+
+func (cs urlCredentialSource) retrieveSubjectToken(c *Config) string {
+	
+}
